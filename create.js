01edd0ee1a7b763ad2ff2c427e07543a071c98ec
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-02-23 12:53:15 -0500

    Create fixes

--- create.js
+++ create.js
@@ -3,18 +3,48 @@
 const mongoose = require('mongoose');
 const connect = require('./db');
 const Voter = require('./schema');
+const readline = require('readline');
+const fs = require('fs');
 
 connect(); // To the database
 
-// read the csv file
-var file = readFile("voters.csv")
-  .then(parse(data){
-    console.log(data);
-  })
-  .catch(error => console.error(error.stack));
 
-
+var allVoters =[];
+// file config
+const file = readline.createInterface({
+  input: fs.createReadStream("voters.csv")
+});
+//asynch line-by-line input
+file.on('line', function(line){
+  var valArr = line.split(",");
+  // need to split voting history
+  var str = valArr[3];
+  var elects = [];
+  if(str !== undefined){
+    for (var i =0; i< str.length; i+=4){
+      elects.push(str.substring(i,i+4));
+    }
+  }
+  const voter = new Voter({
+    first_name: valArr[0],
+    last_name: valArr[1],
+    zip: valArr[2],
+    elections: elects
+  });
+  allVoters.push(voter);
+});
 // Delete any previous data
 mongoose.connection.dropDatabase()
+  .then(function(voters){
+    for(const vote of voters){
+      vote.save()
+    }
+  }
   .then(() => console.log('Database is ready.'))
+  .then(()=>mongoose.connection.close())
   .catch(error => console.error(error.stack));
+
+// end the program
+file.on('close', function(){
+  process.exit(0);
+});
