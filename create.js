5301938eb9c5fafdb850502bc4ea27524b469672
Isaac Brinkman <ibrin16@stlawu.edu>
2020-02-23 17:14:54 -0500

    create

--- create.js
+++ create.js
@@ -36,6 +36,7 @@ file.on('line', function(line) {
   mongoose.connection.dropDatabase()
     .then(() => voter.save())
     .then(() => mongoose.connection.close())
+    .then(() => console.log('db is ready'))
     .catch(error => console.error(error.stack));
 })
 
