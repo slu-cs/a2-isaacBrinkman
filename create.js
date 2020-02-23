4731f572e498bdf59391abc8a4b0cf5d9e1ad8e5
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-02-23 13:32:10 -0500

    return

--- create.js
+++ create.js
@@ -15,39 +15,37 @@ const file = readline.createInterface({
   input: fs.createReadStream("voters.csv")
 });
 //asynch line-by-line input
-console.log("starting",function(){
-    file.on('line', function(line){
-      var valArr = line.split(",");
-      // need to split voting history
-      var str = valArr[3];
-      var elects = [];
-      if(str !== undefined){
-        for (var i =0; i< str.length; i+=4){
-          elects.push(str.substring(i,i+4));
+file.on('line', function(line){
+  var valArr = line.split(",");
+  // need to split voting history
+  var str = valArr[3];
+  var elects = [];
+  if(str !== undefined){
+    for (var i =0; i< str.length; i+=4){
+      elects.push(str.substring(i,i+4));
+    }
+  }
+  const voter = new Voter({
+    first_name: valArr[0],
+    last_name: valArr[1],
+    zip: valArr[2],
+    elections: elects
+  });
+  console.log('time to push voters')
+  allVoters.push(voter)
+  })
+  .then(function(){
+    return mongoose.connection.dropDatabase()
+      .then(()=> console.log('insertion'))
+      .then(function(voters){
+        for(const vote of voters){
+          vote.save()
         }
-      }
-      const voter = new Voter({
-        first_name: valArr[0],
-        last_name: valArr[1],
-        zip: valArr[2],
-        elections: elects
-      });
-      console.log('time to push voters')
-      allVoters.push(voter)
-
       })
-      .then(function(){
-        mongoose.connection.dropDatabase()
-          .then(()=> console.log('insertion'))
-          .then(function(voters){
-            for(const vote of voters){
-              vote.save()
-            }
-          })
-          .then(() => console.log('Database is ready.'))
-          .then(()=>mongoose.connection.close())
-          .catch(error => console.error(error.stack));
-      })
+      .then(() => console.log('Database is ready.'))
+      .then(()=>mongoose.connection.close())
+      .catch(error => console.error(error.stack));
+    })
 });
 
 
