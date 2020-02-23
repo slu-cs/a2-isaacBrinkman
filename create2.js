4228ca34e69535aa7c184f959398c10bb06b292e
Isaac Brinkman <ibrin16@stlawu.edu>
2020-02-23 16:59:57 -0500

    //

--- create2.js
+++ create2.js
@@ -34,7 +34,7 @@ var p1 = new Promise(function(resolve, reject){
       history: elections
     })
     allVoters.push(voter);
-    console.log(allVoters.length);
+    //console.log(allVoters.length);
 })
 console.log("final" + allVoters.length);
 if(allVoters.length > 0){
