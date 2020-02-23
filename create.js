6a3399ef3a046991d5a7c42a2a8cb81c563517b2
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-02-23 13:24:49 -0500

    Promises again

--- create.js
+++ create.js
@@ -15,35 +15,42 @@ const file = readline.createInterface({
   input: fs.createReadStream("voters.csv")
 });
 //asynch line-by-line input
-file.on('line', function(line){
-  var valArr = line.split(",");
-  // need to split voting history
-  var str = valArr[3];
-  var elects = [];
-  if(str !== undefined){
-    for (var i =0; i< str.length; i+=4){
-      elects.push(str.substring(i,i+4));
-    }
-  }
-  const voter = new Voter({
-    first_name: valArr[0],
-    last_name: valArr[1],
-    zip: valArr[2],
-    elections: elects
-  });
-  console.log('time to push voters')
-  allVoters.push(voter);
-  mongoose.connection.dropDatabase()
-    .then(()=> console.log('insertion'))
-    .then(function(voters){
-      for(const vote of voters){
-        vote.save()
+console.log("starting")
+  .then(function(){
+    file.on('line', function(line){
+      var valArr = line.split(",");
+      // need to split voting history
+      var str = valArr[3];
+      var elects = [];
+      if(str !== undefined){
+        for (var i =0; i< str.length; i+=4){
+          elects.push(str.substring(i,i+4));
+        }
       }
-    })
-    .then(() => console.log('Database is ready.'))
-    .then(()=>mongoose.connection.close())
-    .catch(error => console.error(error.stack));
+      const voter = new Voter({
+        first_name: valArr[0],
+        last_name: valArr[1],
+        zip: valArr[2],
+        elections: elects
+      });
+      console.log('time to push voters')
+      allVoters.push(voter)
+
+      })
+      .then(function(){
+        mongoose.connection.dropDatabase()
+          .then(()=> console.log('insertion'))
+          .then(function(voters){
+            for(const vote of voters){
+              vote.save()
+            }
+          })
+          .then(() => console.log('Database is ready.'))
+          .then(()=>mongoose.connection.close())
+          .catch(error => console.error(error.stack));
+      })
 });
+
 
 // end the program
 file.on('close', function(){
