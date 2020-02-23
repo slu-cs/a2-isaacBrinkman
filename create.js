51287cdc8ee2044e350fa63990d9855a2da5a676
Isaac Brinkman <ibrin16@stlawu.edu>
2020-02-23 17:41:44 -0500

    close

--- create.js
+++ create.js
@@ -13,45 +13,36 @@ const file = readline.createInterface({
   input: fs.createReadStream("voters.csv")
 });
 
-const lee = new Voter({
-  firstname: 'Choong-Soo Lee',
-  lastname: 'Associate',
-  zipcode: '2010',
-  elections: [140, 219, 256, 321, 370]
-});
 
 var allVoters = [];
+
 // Asynchronous line-by-line input
-mongoose.connection.dropDatabase()
-/*
-.then(() => {
-  file.on('line', function(line) {
-    var values= line.split(',');
-    // put the election history into an array
-    var str = values[3];
-    var elections = [];
-    if(str !== undefined){ // if they ever voted
-      for (var i = 0; i < str.length; i=i+4) {
-        elections.push(str.substring(i, i+4));
-      }
+file.on('line', function(line) {
+  var values= line.split(',');
+  // put the election history into an array
+  var str = values[3];
+  var elections = [];
+  if(str !== undefined){ // if they ever voted
+    for (var i = 0; i < str.length; i=i+4) {
+      elections.push(str.substring(i, i+4));
     }
-    const voter = new Voter({
-      firstname: values[0],
-      lastname: values[1],
-      zipcode: values[2],
-      history: elections
-    });
-    voter.save();
-    allVoters.push(voter);
-  })
-})
-*/
+  }
+  const voter = new Voter({
+    firstname: values[0],
+    lastname: values[1],
+    zipcode: values[2],
+    history: elections
+  });
+  allVoters.push(voter);
+});
+
+mongoose.connection.dropDatabase()
 .then(() => {
   for(const v of allVoters){
     v.save();
   }
 })
-.then(() => lee.save())
+.then(() => mongoose.connection.close())
 .then(() => console.log('Database is ready'))
 .catch(error => console.error(error.stack));
 
