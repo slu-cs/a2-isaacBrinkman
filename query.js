2f31095446271ec938cd4c282e39de12646f853f
Isaac Brinkman <isaacbrinkman1998@gmail.com>
2020-02-23 14:42:17 -0500

    oops

--- query.js
+++ query.js
@@ -15,6 +15,6 @@ const queries = [
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(results) {
-    console.log('Number of voters in Canton', results.length);
+    console.log('Number of voters in Canton', results[0].length);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
